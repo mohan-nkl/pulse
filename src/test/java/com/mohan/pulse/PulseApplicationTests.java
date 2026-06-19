9b875ca5ee6e56e45139f72b6bf94d7d086052ff
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-06-19 11:03:51 +0530

    Initial project setup

---
+++ src/test/java/com/mohan/pulse/PulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan.pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
