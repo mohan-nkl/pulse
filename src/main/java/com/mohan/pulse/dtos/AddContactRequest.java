bd4a06d9522515cbb6704b68519d3d4fc37cd5ce
sarfras <sarfras2001@gmail.com>
2026-06-22 17:11:45 +0530

    fix: add @Setter to contact DTOs for Jackson deserialization

--- src/main/java/com/mohan/pulse/dtos/AddContactRequest.java
+++ src/main/java/com/mohan/pulse/dtos/AddContactRequest.java
@@ -3,9 +3,11 @@ package com.mohan.pulse.dtos;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-  @NoArgsConstructor                                                                                                                                                               
+@Setter
+@NoArgsConstructor                                                                                                                                                               
   public class AddContactRequest {                                                                                                                                                 
                                                                                                                                                                                    
       @NotBlank(message = "Phone number is required.")                                                                                                                             
